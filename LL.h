9ea5460a98eac1aa4d09e58f32433ed1328527df
RakaOtaku <xhuagf299@gmail.com>
2024-05-25 23:00:30 +0700

    Pertama

---
+++ LL.h
@@ -0,0 +1,288 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+
+using namespace std;
+
+const int max_data = 99;
+
+// ?? ??? ???? ????? LINKED LIST ????? ???? ??? ?? //
+
+//Linked List Pulsa
+struct Item
+{
+    string name;
+    int price;
+    int id;
+    Item *next;
+    Item *prev;
+};
+
+//Defined? apa namanya gatau pokoknya bikin variabel
+Item *head;
+Item *tail;
+Item *pulsa;
+Item *current;
+Item *wanted;
+
+//Buat Node Baru
+void buat_pulsa(string nama, int harga, int id)
+{
+    pulsa = new Item;
+    pulsa->name = nama;
+    pulsa->price = harga;
+    pulsa->id = id;
+    pulsa->next = NULL;
+    pulsa->prev = NULL;
+}
+
+//Inisialisasi
+void inisialisasi_pulsa() {
+    head == NULL;
+    tail == NULL;
+}
+
+//var isEmpty
+bool kosong() {
+    if (head == NULL && tail == NULL) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Buat Jadiin CDLL gitu? gatau juga kalo sepemahamanku gt
+void tambahp(string nama, int harga, int id)
+{
+    buat_pulsa(nama, harga, id);
+
+    if (kosong()) {
+        head = pulsa;
+        tail = pulsa;
+    } else {
+        tail->next = pulsa;
+        pulsa->prev = tail;
+        tail = pulsa;
+    }
+
+    tail->next = head;
+    head->prev = tail;
+}
+
+//Edit Data
+void editp(int cari) {
+    if (kosong()) {
+        cout << "Tidak ada data." << endl;
+    } else {
+        wanted = head;
+        
+        while (wanted->next != head && wanted->id != cari) {
+            wanted = wanted->next;
+        }
+
+        if (wanted->id != cari) {
+            cout << "Data tidak ada." << endl;
+        } else {
+            cout << "Masukkan Nama : ";
+            cin.ignore();
+            getline(cin, wanted->name);
+            cout << "Masukkan Harga : ";
+            cin >> wanted->price;
+            cout << "Masukkan Id : ";
+            cin >> wanted->id;
+
+            cout << "\nData Berhasil diUpdate." << endl;
+        }
+    }
+}
+
+//Hapus Data
+void hapusp (int cari) {
+    if (kosong()) {
+        cout << "Tidak ada data." << endl;
+    } else {
+        wanted = head;
+
+        while (wanted->next != head && wanted->id != cari) {
+            wanted = wanted->next;
+        }
+
+        if (wanted->id != cari) {
+            cout << "Data tidak ada." << endl;
+        } else if (wanted == head && wanted == tail) {
+            head = tail = NULL;
+            cout << "\nData berhasil dihapus. " << endl;
+        } else {
+            if (wanted == head) {
+                head = wanted->next;
+                tail->next = head;
+                head->prev = tail;
+            } else if (wanted == tail) {
+                tail = wanted->prev;
+                tail->next = head;
+                head->prev = tail;
+            } else {
+                wanted->prev->next = wanted->next;
+                wanted->next->prev = wanted->prev;
+            }
+            delete wanted;
+            cout << "\nData berhasil dihapus. " << endl;
+        }
+    }
+}
+
+//Ya masa gatau
+void tampil_pulsa_admin() {
+    if (kosong() == true) {
+        cout << "Tidak ada data." << endl;
+    } else {
+        current = head;
+        do {
+            cout << "Data : " << current->name << endl;
+            cout << "Harga : " << current->price << endl;
+            cout << "Id : " << current->id << endl;
+            current = current->next;
+        } while (current!= head);
+    }
+}
+
+//Ya masa gatau
+void tampil_pulsa() {
+    if (kosong() == true) {
+        cout << "Tidak ada data." << endl;
+    } else {
+        current = head;
+        do {
+            cout << "Data : " << current->name << " Harga : " << current->price << endl;
+            current = current->next;
+        } while (current!= head);
+    }
+}
+
+
+
+// ?? ??? ???? ????? REKENING (ARRAY) ????? ???? ??? ?? //
+
+
+string BCA[99] = {"1223456789 A/N RAKA ANDRIY SHEVCHENKO", "123456789 A/N P", "123456789 A/N Q", ""};
+string GP[99] = {"1223456789 A/N RAKA ANDRIY SHEVCHENKO", "123456789 A/N P", "123456789 A/N Q", ""};
+string DN[99] = {"1223456789 A/N RAKA ANDRIY SHEVCHENKO", "123456789 A/N P", "123456789 A/N Q"};
+
+//Ngitung Data
+int countNonEmpty(const string arr[]) {
+    int count = 0;
+    while (arr[count] != "") {
+        ++count;
+    }
+    return count;
+}
+
+
+//Ambil Data
+void rek() {
+    int jBCA = countNonEmpty(BCA);
+    int jGP = countNonEmpty(GP);
+    int jDN = countNonEmpty(DN);
+    // Do something with the collected data, if needed
+}
+
+//Masa gatau
+void tampil_BCA() {
+    int jBCA = countNonEmpty(BCA);
+    for (int i = 0; i < jBCA; i++) {
+        cout << BCA[i] << endl;
+    }
+}
+
+//Masa gatau
+void tampil_GP() {
+    int jGP = countNonEmpty(GP);
+    for (int i = 0; i < jGP; i++) {
+        cout << GP[i] << endl;
+    }
+}
+
+//Masa gatau
+void tampil_DN() {
+    int jDN = countNonEmpty(DN);
+    for (int i = 0; i < jDN; i++) {
+        cout << DN[i] << endl;
+    }
+}
+
+
+// ?? ??? ???? ????? KERANJANG (STACK) ????? ???? ??? ?? //
+
+int total = 0;
+
+//Struct Keranjang
+struct Keranjang
+{
+    string nama_data[max_data];
+    int harga_produk[max_data];
+    int atas = -1;
+};
+Keranjang cart;
+
+void inisialisasi_keranjang()
+{
+    head = NULL;
+    tail = NULL;
+}
+
+bool kosongk() {
+    if (cart.atas == -1) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+bool fullk() {
+    if (cart.atas == max_data) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void tambahk(int cari) {
+    wanted = head;
+    while (wanted != NULL && wanted->id != cari) {
+        wanted = wanted->next;
+    }
+
+    if (wanted == NULL) {
+        cout << "Data tidak ditemukan." << endl;
+    } else if (!fullk()) {
+        cart.atas++;
+        cart.nama_data[max_data] = wanted->name;
+        cart.harga_produk[max_data] = wanted->price;
+        total += cart.harga_produk[max_data];
+
+        cout << "Data telah ditambahkan ke keranjang.";
+    }
+}
+
+void hapusk() {
+    if (!kosongk()) {
+        cout << "Data berhasil dihapus." << endl;
+        cart.atas--;
+    } else {
+        cout << "Data Kosong." << endl;
+    }
+}
+
+void tampilk () {
+    cout << "Isi Keranjang Anda : " << endl;
+    if (!kosong()) {
+        int count = 1;
+        for (int i = cart.atas; i >= 0; i--) {
+            cout << count << ". " << cart.nama_data[i] << " " << cart.harga_produk[i];
+            count++;
+        }
+        cout << "Total Belanjaanmu : " << total << endl;
+    } else {
+        cout << "Keranjangmu masih kosong nih" << endl;
+    }
+}
---
+++ menu.cpp
@@ -0,0 +1,196 @@
+#include "LL.h"
+
+//Submenu 1 (Data)
+void menu1(int pilih) {
+    menu1:
+    int pilihan;
+    cout << ">>>>> Sub-Menu Pulsa <<<<<" << endl;
+    cout << '\n';
+    tampil_pulsa_admin();
+    cout << "\n1. Tambah Pulsa" << endl;
+    cout << "2. Edit Pulsa" << endl;
+    cout << "3. Hapus Pulsa" << endl;
+    cout << "Masukkan pilihan : ";
+    cin >> pilihan;
+
+    if (pilihan == 1) {
+        string name;
+        int price;
+        int id;
+        
+        cout << "Masukkan Nama Pulsa : ";
+        cin.ignore();
+        getline(cin, name);
+        cout << "Masukkan Harga : ";
+        cin >> price;
+        cout << "Masukkan Id : ";
+        cin >> id;
+
+        tambahp(name, price, id);
+        goto menu1;
+    } else if (pilihan == 2) {
+        int cari;
+
+        cout << "Masukkan id Data yang ingin diubah : ";
+        cin >> cari;
+        
+        editp(cari);
+        goto menu1;
+    } else if (pilihan == 3) {
+        int cari;
+
+        cout << "Masukkan id data yang ingin dihapus : ";
+        cin >> cari;
+
+        hapusp(cari);
+        goto menu1;
+    } else {
+        cout << "Harap pilih pilihan yang tersedia." << endl;
+        goto menu1;
+    }
+}
+
+//Submenu 2 (Rekening Tujuan)
+void menu2(int pilih) {
+    menu2:
+    int pilihan;
+    cout << ">>>>> Sub-Menu Rekening Tujuan <<<<<" << endl;
+    cout << '\n';
+    cout << "BCA : " << endl;
+    rek();
+    tampil_BCA();
+    cout << '\n';
+    cout << "Gopay : " << endl;
+    rek();
+    tampil_GP();
+    cout << '\n';
+    cout << "Dana : " << endl;
+    rek();
+    tampil_DN();
+    cout << "\n1. Tambah Rekening" << endl;
+    cout << "2. Edit Rekening" << endl;
+    cout << "3. Hapus Rekening" << endl;
+    cout << "Masukkan pilihan : ";
+    cin >> pilihan;
+}
+
+//Ya gitu
+void menu_admin() {
+    system ("cls");
+    int pilih;
+    cout << "Menu Admin ARRZ" << endl;
+    cout << "1. Edit Pulsa" << endl;
+    cout << "2. Edit Rekening" << endl;
+    cout << "Masukkan pilihan : ";
+    cin >> pilih;
+    
+    if (pilih == 1) {
+        menu1(1);
+    } else if (pilih == 2) {
+        menu2(2);
+    }
+}
+
+//Sama aja
+void menu_user() {
+    int pilihu;
+    system ("cls");
+    cout << "===== Selamat Datang di Counter ARRZ =====" << endl;
+    cout << "Pulsa Yang Tersedia : " << endl;
+    cout << '\n';
+    tampil_pulsa();
+    cout << '\n';
+    cout << "1. Beli Pulsa" << endl;
+    cout << "2. Cek Keranjang" << endl;
+    cout << "3. Bayar" << endl;
+    cout << "Masukkan Pilihan Anda : ";
+    cin >> pilihu;
+
+    switch (pilihu) {
+        case 1:
+        int carip;
+        cout << "Masukkan Nomor Item : ";
+        cin >> carip;
+        tambahk(carip);
+        break;
+
+        case 2: 
+        tampilk();
+        break;
+
+        case 3:
+        int pilihm;
+        cout << "Pilih Metode Pembayaran : " << endl;
+        cout << "1. BCA" << endl;
+        cout << "2. Gopay" << endl;
+        cout << "3. Dana" << endl;
+        cout << "Masukkan pilihan anda : ";
+        cin >> pilihm;
+        
+        if (pilihm == 1) {
+            rek();
+            tampil_BCA();
+        } else if (pilihm == 2) {
+            rek();
+            tampil_GP();
+        } else if (pilihm == 3) {
+            rek();
+            tampil_DN();
+        }
+        
+        default:
+        cout << "Harap pilih sesuai pilihan.";
+        break;
+    }
+}
+
+//Login kang
+void admin() {
+    string username = "arrz";
+    string password = "123";
+    string user;
+    string pass;
+    cout << "\nAdmin ARRZ" << endl;
+    cout << "Username: ";
+    cin >> user;
+    if (user == username){
+        cout << "Password: ";
+        cin >> pass;
+        if (pass == password){
+            cout << "Berhasil login, akan redirect ke menu admin." << endl;
+            sleep(2);
+            system("cls");
+            menu_admin();
+        }
+        else {
+            cout << "Incorrect Password!" << endl;
+        }
+    }
+    else {
+        cout << "Incorrect Username!" << endl;
+    }   
+}
+
+
+int main(int argc, char const *argv[])
+{
+    system("cls");
+    tambahp("Raka", 5000, 1);
+    awal:
+    int pilih;
+    cout << ">>>>> E-Counter ARRZ <<<<<" << endl;
+    cout << "Login Sebagai: " << endl;
+    cout << "1. Admin\n2. User" << endl;
+    cout << "Masukkan Pilihan : ";
+    cin >> pilih;
+
+    if (pilih == 1) {
+        admin();
+        goto awal;
+    } else if (pilih == 2) {
+        menu_user();
+        goto awal;
+    }
+    
+    return 0;
+}
